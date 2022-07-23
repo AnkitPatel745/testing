20d21832b00c5b6ce22febe519b05eca49698cd0
AnkitPatel745 <ankitem745@gmail.com>
2022-07-23 23:35:35 +0530

    added

--- printstatement.py
+++ printstatement.py
@@ -1,3 +1,2 @@
-print("Hello to all this is my first code that i push it from vs code")
 print ("Welcome to the world of coding")
 print ("I am very happy with knowledge what i am gaining form this Course")
